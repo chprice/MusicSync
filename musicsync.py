8a96966cb47ed6c7b8239d05f05c08c8af7f23da
Channon Price <chprice@csh.rit.edu>
2013-06-28 08:30:14 -0800

    Resolved issue with networking
    Files now send successfully, there is some checking to see if the pickle
    file is corrupted. Added code to do moves on non-dump files.

--- musicsync.py
+++ musicsync.py
@@ -3,6 +3,8 @@ import time
 import re
 import hashlib
 import sys
+import pickle
+import socket
 
 def makeHash(fileName): #given a filename and path, computes hash of file
     tHash = hashlib.sha256()
@@ -15,6 +17,7 @@ def makeHash(fileName): #given a filename and path, computes hash of file
 
 class tree():
     def __init__(self, path):
+        self.path = path
         self.hashtable = dict()
         self.addChild(path)
     def addChild(self, path):
@@ -22,7 +25,7 @@ class tree():
         folders = [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]
         for phile in files:
             if(len(re.findall(".*(mp3|wav|mp4)+", phile)) != 0):
-                self.hashtable[makeHash(os.path.join(path, phile))] = os.path.join(path, phile)
+                self.hashtable[makeHash(os.path.join(path, phile))] = os.path.join(path, phile).replace(self.path, '')
         for folder in folders:
             self.addChild(os.path.join(path, folder))
     def compare(self, otherTree):
@@ -75,22 +78,43 @@ def downloadTree(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
     s.send('h')
-    size = s.recv(20)
+    size = s.recv(30)
     pickledTree = s.recv(int(size))
+    s.send('q')
+    s.close()
     return pickle.loads(pickledTree)
 
-def downloadSongs(host, port, actions):
+def downloadSongs(host, port, actions, path):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    for songHash, songLocation in actions:
-        songHandle = open(songLocation, 'wb')
-        s.connect((host,port))
+    s.connect((host,port))
+    for songHash, songLocation in actions['added']:
+        sys.stdout.write("Getting " + songLocation + ' ')
+        songHandle = open(path + songLocation, 'wb')
         s.send('s')
         s.send(songHash)
-        size = s.recv(20)
-        song = s.recv(int(size))
-        songHandle.write(song)
-        songHandle.close()
-        s.close()
+        size = int(s.recv(30))
+        song = ''
+        percent = 1
+        while(len(song) + 1024 < size):
+            chunk = s.recv(1024)
+            song += chunk
+            if(len(song) >= percent * size/10.0):
+                sys.stdout.write('#')
+                percent += 1
+        song += s.recv(size % 1024)
+        print '#'
+        lines = ''
+        try:
+            lines = pickle.loads(song)
+        except:
+            print 'File not sent completely. Song not written to disk.'
+        if(True): #do a hash check
+            songHandle.writelines(lines)
+            songHandle.close()
+        else:
+            print "Hash mismatch. Song not written to disk."
+    s.send('q')
+    s.close()
 
 def serveFiles(port, folder):
     print "Building tree."
@@ -105,32 +129,35 @@ def serveFiles(port, folder):
     while True:
         newsocket, fromaddr = bindsocket.accept()
         try:
-            print "Serving "+fromaddr
-            handleRequest(newsocket, folder, tree)
+            print "Serving " #+ fromaddr
+            handleRequest(newsocket, pTree, tree)
         finally:
             newsocket.shutdown(socket.SHUT_RDWR)
             newsocket.close()
         
 def handleRequest(s, pTree, tree):
-    op = s.recv(1)
-    if(op == 'h'): #hashtree
-        s.send(fit(len(pTree)))
-        s.send(pTree)
-        return
-
-    if(op == 's'): #song
-        fileHash = stream.recv(64) #receive hash of file
-        location = tree.hashtable[fileHash]
-        size = os.path.getsize(location)
-        song = open(location, 'rb')
-        s.send(fit(size))
-        s.send(song)
-        return
+    while(True):
+        op = s.recv(1)
+        if(op == 'h'): #hashtree
+            s.send(fit(len(pTree)))
+            s.send(pTree)
+        if(op == 's'): #song
+            fileHash = s.recv(64) #receive hash of file
+            location = tree.path + tree.hashtable[fileHash]
+            print "Sending", location
+            songHandle = open(location, 'rb')
+            song = songHandle.readlines()
+            songHandle.close()
+            pickledSong = pickle.dumps(song)
+            s.send(fit(len(pickledSong)))
+            s.send(pickledSong)
+        if(op == 'q'):
+            return
 
 
 def fit(number):
     string = str(number)
-    while(len(string) < 20):
+    while(len(string) < 30):
         string = '0' + string
     return string
         
@@ -149,7 +176,7 @@ def fit(number):
 
 
 args = sys.argv[1:][::-1]
-hasHost, hasPort, hasDump, hasFolder, hasAdd, download, delete, isServer  = False, False, False, False, False, False, False, False
+hasHost, hasPort, hasDump, hasFolder, hasAdd, download, delete, isServer  = False, False, False, False, False, True, True, False
 while(len(args) != 0):
     flag = args.pop()
     if(flag == "-h" or flag == "-host"):
@@ -180,28 +207,28 @@ while(len(args) != 0):
 
 if(not hasPort):
     port = 7355
-if(isServer and hasFolder):
-    serveFiles(port, folder)
-if(hasAdd and hasHost):
-    actions = {}
-    actions["added"] = loadAdded(path)
-    downloadSongs(host, port, actions)
-    sys.exit(0)
-if(not hasAdd and not hasFolder):
+if(not hasFolder):
     print "Folder not defined"
     sys.exit(1)
+if(isServer and hasFolder):
+    serveFiles(port, folder)
+if(hasAdd and hasHost and hasFolder):
+    actions = {}
+    actions["added"] = loadAdded(addedFile)
+    downloadSongs(host, port, actions, folder)
+    sys.exit(0)
 if(hasDump):
     old = buildTree(folder)
     new = loadTree(hashDump)
     actions = old.compare(new)
     for fileData in actions["moved"]:
-        os.rename(fileData[1], fileData[2])
+        os.rename(old.path + fileData[1], old.path + fileData[2])
     if(delete):
         for fileData in actions["removed"]:
-            os.remove(fileData[1])
+            os.remove(old.path + fileData[1])
     if(hasHost):
         if(download):
-            downloadSongs(host, port, actions)
+            downloadSongs(hostname, port, actions, folder)
             sys.exit(0)
         else:
             dumpAdded(actions)
@@ -210,14 +237,16 @@ if(hasDump):
         dumpAdded(actions)
         sys.exit(0)
 if(hasHost):
-    new = downloadTree(host, port)
+    new = downloadTree(hostname, port)
     old = buildTree(folder)
     actions = old.compare(new)
+    for fileData in actions["moved"]:
+        os.rename(old.path + fileData[1], old.path + fileData[2])
     if(delete):
         for fileData in actions["removed"]:
-            os.remove(fileData[1])
+            os.remove(old.path + fileData[1])
     if(download):
-        downloadSongs(host, port, actions)
+        downloadSongs(hostname, port, actions, folder)
         sys.exit(0)
     else:
         dumpAdded(actions)
